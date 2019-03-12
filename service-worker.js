c7fd5e270502c8106fbfd77e04913b6220dbb34c
WenqingWu <wenqingwu2015@gmail.com>
2019-03-12 17:02:04 +0800

    Site updated: 2019-03-12 17:02:04

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["2018/07/17/hexo introduction/index.html","2fb88a1f157fed6e5b34c138a62aeab0"],["2018/07/18/mTCP-Code-Reading/index.html","4331cc3d510ddbb89e9b8d9d59c174ab"],["2018/07/18/mTCP-Code-Reading/mtcp-epserver-wu.png","637ea841131b01dad9ceaa0e3089b94b"],["2018/07/26/C语言开发调试经验记录/1.png","a91685fd3dd89dd504338258f5a04de4"],["2018/07/26/C语言开发调试经验记录/index.html","a0d1ce23dff7dd29b9ebb1950d61c03d"],["2018/09/29/Linux-时间函数小结/index.html","c9af1017d7aaaba19445a04499838c22"],["2018/12/08/pdump-DPDK抓包工具/index.html","e8e11adf8dba418ab214d9d6135e14d7"],["2018/12/08/pdump-DPDK抓包工具/pdump-framework.png","56bd02031cba73fcb9edd8ab589bf9c8"],["2018/12/08/pdump-DPDK抓包工具/pdump-process.png","eaa9b587f93d13610d78e7fa300b88d3"],["archives/2018/07/index.html","e23386cea9b050bacdaae7fc2649d45e"],["archives/2018/09/index.html","9dff78bc6a7c8e32ec7807b446d1a840"],["archives/2018/12/index.html","edc1d455b2bd2991c1d0d23c877ebf49"],["archives/2018/index.html","786ceecedd6d9b490c107282f35baba8"],["archives/index.html","786ceecedd6d9b490c107282f35baba8"],["categories/technology/index.html","a3cca67fb7d2f5f512dbddb7d7a909e6"],["css/default.css","e5e684d21cb02e9f7e81a2b722bddafc"],["css/donate.css","a68e4a34e814ce3334186286f4655440"],["css/style.css","3496e1c7588e0ae31148a801b01219f7"],["donate/index.html","91503525d9362bc2db45b1102e43690a"],["google9b8d8a81b13d16eb.html","09e035849640b21d046f78559d13b08e"],["img/AliPayQR.png","1511d0c6f91582f2b7bf20d9b0956f87"],["img/BTCQR.png","a48f0b8d3aa8c07adbeffe2979b2e5a4"],["img/WeChatQR.png","8369323e99c4c96518e1297b47a71428"],["img/alipay.svg","9239702087add999b29eda6c69b7fac3"],["img/bitcoin.svg","73007d7f0ee6052c13edec23f0ee5c78"],["img/github.svg","23fc8f81f92bb2981d8f9e089d7df14a"],["img/like.svg","335eff6a0aefd9ce25d8624c9cae2f54"],["img/paypal.svg","96fa023e7e12051f7585b6fe4da53daf"],["img/wechat.svg","f9bcef76a75dae0e4fe6bf3d3af1cad3"],["index.html","4d16edd6a33b7061e7213cb81d25cb23"],["js/codeblock-resizer.js","f70200751db2c1b3cff95884d7ecc132"],["js/donate.js","c5119c5bdd5049d805e03af774a47472"],["js/fancybox.js","7a432f63d671adfa0cb9a5458c6db4a8"],["js/gitment.browser.js","434949e3fa004587f73aada62910cfc4"],["js/search.js","2d82d5f1f7964cab86b4df17fcc946f9"],["js/share.js","013db0d5d1e2a227ac821fe8f0af678d"],["js/smartresize.js","14d842f8dea14c352b055b4e9de5cf07"],["js/totop.js","4ec0df4fc761d8a5433c8f0ba94750f7"],["tags/C/index.html","5832add2993673205cdb827db795ff0c"],["tags/DCN/index.html","c71c195303c7533ab1d30d0598779229"],["tags/DPDK/index.html","af8ab0ba88e8cab8ca6817103e04266c"],["tags/Linux-DPDK-DCN/index.html","54d195edcadaf026f52c018d6aeea25e"],["tags/Linux/index.html","9b7f07d2c01ac46a28b6f84878d5cedf"],["tags/code/index.html","bbea8e15ba25c8cd026e9da49f4fec4e"],["tags/hexo/index.html","a68a70fa8ec308b2cf2a6a6663dab789"],["tags/mTCP/index.html","a505200f0b157c543433b410aed5c5cc"],["tags/technology/index.html","3a12db25f7d12906ce050d3ff8aa57d7"]];
+var precacheConfig = [["2018/07/17/hexo introduction/index.html","089998e5115fa7b74a711b1f919715a7"],["2018/07/18/mTCP-Code-Reading/index.html","40a958d8c7fb2e1628300a55ad2f6887"],["2018/07/18/mTCP-Code-Reading/mtcp-epserver-wu.png","637ea841131b01dad9ceaa0e3089b94b"],["2018/07/26/C语言开发调试经验记录/1.png","a91685fd3dd89dd504338258f5a04de4"],["2018/07/26/C语言开发调试经验记录/index.html","a8183fa86d997c7c47bd08228d5b1d3d"],["2018/09/29/Linux-时间函数小结/index.html","24c3044728f47dcbae00f4993bfd97e6"],["2018/12/08/pdump-DPDK抓包工具/index.html","fa457dba9e5ff8744742cd699eb9ce15"],["2018/12/08/pdump-DPDK抓包工具/pdump-framework.png","56bd02031cba73fcb9edd8ab589bf9c8"],["2018/12/08/pdump-DPDK抓包工具/pdump-process.png","eaa9b587f93d13610d78e7fa300b88d3"],["2018/12/10/Linux-下的PF-PACKET和SOCK-RAW/index.html","828e83d2ea40832c9ffad0542ccf32cd"],["archives/2018/07/index.html","a06314ff1fba8f549163486f4c969936"],["archives/2018/09/index.html","b04edbd36e6bb3b36873997436a4fb0c"],["archives/2018/12/index.html","4eb05775b20a646222d9cbd5da476f3e"],["archives/2018/index.html","c271c9244697f4d79e7fb516a4f7713e"],["archives/index.html","c271c9244697f4d79e7fb516a4f7713e"],["categories/technology/index.html","69a90a591c150667cc6a2632b29e6de9"],["css/default.css","e5e684d21cb02e9f7e81a2b722bddafc"],["css/donate.css","a68e4a34e814ce3334186286f4655440"],["css/style.css","3496e1c7588e0ae31148a801b01219f7"],["donate/index.html","91503525d9362bc2db45b1102e43690a"],["google9b8d8a81b13d16eb.html","430d92b344a13b9ddfbcc9414fe35c46"],["img/AliPayQR.png","1511d0c6f91582f2b7bf20d9b0956f87"],["img/BTCQR.png","a48f0b8d3aa8c07adbeffe2979b2e5a4"],["img/WeChatQR.png","8369323e99c4c96518e1297b47a71428"],["img/alipay.svg","9239702087add999b29eda6c69b7fac3"],["img/bitcoin.svg","73007d7f0ee6052c13edec23f0ee5c78"],["img/github.svg","23fc8f81f92bb2981d8f9e089d7df14a"],["img/like.svg","335eff6a0aefd9ce25d8624c9cae2f54"],["img/paypal.svg","96fa023e7e12051f7585b6fe4da53daf"],["img/wechat.svg","f9bcef76a75dae0e4fe6bf3d3af1cad3"],["index.html","a3767ffe11e47f4efc209f4e431e0b01"],["js/codeblock-resizer.js","f70200751db2c1b3cff95884d7ecc132"],["js/donate.js","c5119c5bdd5049d805e03af774a47472"],["js/fancybox.js","7a432f63d671adfa0cb9a5458c6db4a8"],["js/gitment.browser.js","434949e3fa004587f73aada62910cfc4"],["js/search.js","2d82d5f1f7964cab86b4df17fcc946f9"],["js/share.js","013db0d5d1e2a227ac821fe8f0af678d"],["js/smartresize.js","14d842f8dea14c352b055b4e9de5cf07"],["js/totop.js","4ec0df4fc761d8a5433c8f0ba94750f7"],["tags/C/index.html","798af18b06f3260948db22520c5ff894"],["tags/DCN/index.html","1968aafe74fa7176f29c9d698f58d721"],["tags/DPDK/index.html","bb5e0affea94fede869e0e333cdcf005"],["tags/Linux-DPDK-DCN/index.html","7811f313612533bcec7673da51f7831c"],["tags/Linux-TCP-IP-DCN/index.html","84bafb23e72f398b5b7d16d504244ee2"],["tags/Linux/index.html","476de6dfa04fa8cb6b4a2dd7380932e5"],["tags/code/index.html","bbea8e15ba25c8cd026e9da49f4fec4e"],["tags/hexo/index.html","c370fdc0994133f0f91c79b6eb42fbc2"],["tags/mTCP/index.html","e2b261fb9d8c09c08bb2b9d3516da143"],["tags/technology/index.html","2e11111df3006f511dac3775b3d83adc"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
@@ -45,7 +45,7 @@ var ignoreUrlParametersMatching = [/^utm_/];
 
 
 
-var addDirectoryIndex = function (originalUrl, index) {
+var addDirectoryIndex = function(originalUrl, index) {
     var url = new URL(originalUrl);
     if (url.pathname.slice(-1) === '/') {
       url.pathname += index;
@@ -53,7 +53,7 @@ var addDirectoryIndex = function (originalUrl, index) {
     return url.toString();
   };
 
-var cleanResponse = function (originalResponse) {
+var cleanResponse = function(originalResponse) {
     // If this is not a redirected response, then we don't have to do anything.
     if (!originalResponse.redirected) {
       return Promise.resolve(originalResponse);
@@ -75,7 +75,7 @@ var cleanResponse = function (originalResponse) {
     });
   };
 
-var createCacheKey = function (originalUrl, paramName, paramValue,
+var createCacheKey = function(originalUrl, paramName, paramValue,
                            dontCacheBustUrlsMatching) {
     // Create a new URL object to avoid modifying originalUrl.
     var url = new URL(originalUrl);
@@ -91,7 +91,7 @@ var createCacheKey = function (originalUrl, paramName, paramValue,
     return url.toString();
   };
 
-var isPathWhitelisted = function (whitelist, absoluteUrlString) {
+var isPathWhitelisted = function(whitelist, absoluteUrlString) {
     // If the whitelist is empty, then consider all URLs to be whitelisted.
     if (whitelist.length === 0) {
       return true;
@@ -104,7 +104,7 @@ var isPathWhitelisted = function (whitelist, absoluteUrlString) {
     });
   };
 
-var stripIgnoredUrlParameters = function (originalUrl,
+var stripIgnoredUrlParameters = function(originalUrl,
     ignoreUrlParametersMatching) {
     var url = new URL(originalUrl);
     // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
